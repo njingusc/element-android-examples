0633ad3d4941daf5a96823ba71ba2deb17a98aac
Tyler <tt@discoverelement.com>
2019-03-18 18:14:04 -0700

    Added Cordova FM example

---
+++ element-cordova-face-matching-example/www/js/index.js
@@ -0,0 +1,42 @@
+
+function successCallback(message){
+    alert("Success -> " + message);
+    document.getElementById('id').value = "";
+}
+
+function errorCallback(message){
+    alert("Failure -> " + message);
+}
+
+var app = {
+    // Application Constructor
+    initialize: function() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    // deviceready Event Handler
+    //
+    // Bind any cordova events here. Common events are:
+    // 'pause', 'resume', etc.
+    onDeviceReady: function() {
+        this.receivedEvent('deviceready');
+        document.getElementById('capture').addEventListener('click', function(){
+            var id = document.getElementById('id').value;
+            element.capture(id, successCallback, errorCallback);
+        });
+    },
+
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
+
+app.initialize();
